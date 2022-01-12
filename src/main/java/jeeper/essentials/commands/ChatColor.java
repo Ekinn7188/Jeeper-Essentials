e2941f0cbba40bdb24a0421d2147488d13884755
ekinn7188 <ekinn7188@gmail.com>
2022-01-12 09:16:11 -0800

    Chatcolor applies to only the user who typed the command

--- src/main/java/jeeper/essentials/commands/ChatColor.java
+++ src/main/java/jeeper/essentials/commands/ChatColor.java
@@ -2,6 +2,7 @@ package jeeper.essentials.commands;
 
 import essentials.db.Tables;
 import jeeper.essentials.Main;
+import jeeper.essentials.database.DatabaseTools;
 import jeeper.utils.MessageTools;
 import jeeper.utils.config.ConfigSetup;
 import net.kyori.adventure.text.Component;
@@ -27,9 +28,16 @@ public class ChatColor extends PluginCommand {
     public void execute(Player player, String[] args) {
 
         if (args.length > 0) {
-
             if (!MessageTools.parseText(args[0]).equals(Component.text(args[0]))) {
-                dslContext.update(Tables.USERS).set(Tables.USERS.CHATCOLOR, args[0]).execute();
+                int userID = DatabaseTools.getUserID(player.getUniqueId());
+
+                if (userID == -1) {
+                    DatabaseTools.addUser(player.getUniqueId());
+                    userID = DatabaseTools.getUserID(player.getUniqueId());
+                }
+
+                dslContext.update(Tables.USERS).set(Tables.USERS.CHATCOLOR, args[0])
+                        .where(Tables.USERS.USERID.eq(userID)).execute();
                 player.sendMessage(MessageTools.parseFromPath(config, "Chat Color Set"));
 
             } else {
