472656bf1ae4860534798c4ff909c29808cdbcc7
Evan K <30984495+Ekinn7188@users.noreply.github.com>
2022-07-06 20:01:34 -0700

    Armor stand limit only applies to players, not commands

--- src/main/java/jeeper/essentials/listeners/ArmorStand.java
+++ src/main/java/jeeper/essentials/listeners/ArmorStand.java
@@ -1,11 +1,12 @@
 package jeeper.essentials.listeners;
 
-import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
 import io.papermc.paper.event.entity.EntityMoveEvent;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
@@ -15,6 +16,7 @@ public class ArmorStand implements Listener {
 
     @EventHandler
     public void onEntityMove(EntityMoveEvent e) {
+        if (!e.getEntity().getEntitySpawnReason().equals(CreatureSpawnEvent.SpawnReason.DEFAULT)) return;
         if (e.getEntity().getType() == org.bukkit.entity.EntityType.ARMOR_STAND) {
             if (!e.getTo().getChunk().equals(e.getFrom().getChunk())) {
                 int standCount = (int) Arrays.stream(e.getEntity().getLocation().getChunk().getEntities()).filter(entity -> entity.getType() == org.bukkit.entity.EntityType.ARMOR_STAND).count();
@@ -27,16 +29,16 @@ public class ArmorStand implements Listener {
         }
     }
 
-
     @EventHandler
-    public void onEntityAddToWorld(EntityAddToWorldEvent e) {
-        if (e.getEntity().getType() == org.bukkit.entity.EntityType.ARMOR_STAND) {
-            Chunk chunk = e.getEntity().getLocation().getChunk();
-            int standCount = (int) Arrays.stream(chunk.getEntities()).filter(entity -> entity.getType() == org.bukkit.entity.EntityType.ARMOR_STAND).count();
-            if (standCount >= 6) {
-                e.getEntity().getWorld().dropItemNaturally(e.getEntity().getLocation(), new ItemStack(Material.ARMOR_STAND));
-                e.getEntity().remove();
-            }
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        if (e.getClickedBlock() == null) return;
+        if (!e.getMaterial().equals(Material.ARMOR_STAND)) return;
+        Chunk chunk = e.getClickedBlock().getLocation().getChunk();
+        var armorStands = Arrays.stream(chunk.getEntities()).filter(entity -> entity.getType() == org.bukkit.entity.EntityType.ARMOR_STAND).toList();
+        int standCount = armorStands.size();
+        if (standCount >= 6) {
+            e.getClickedBlock().getWorld().dropItemNaturally(e.getClickedBlock().getLocation(), new ItemStack(Material.ARMOR_STAND));
+            e.setCancelled(true);
         }
     }
 }
